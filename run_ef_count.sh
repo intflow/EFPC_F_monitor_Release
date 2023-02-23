93af2101418c2a25dfef52f6a5e4bece8270790e
inchulryu <inchuuul.ryu@gmail.com>
2023-02-23 17:36:45 +0900

    monitor log added

--- run_ef_count.sh
+++ run_ef_count.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+LOG_FILE=/home/intflow/works/logs/$(date +%Y%m%d_%H%M%S)_monitor.log
+
 sudo jetson_clocks
 sudo sh -c 'echo 150 > /sys/devices/pwm-fan/target_pwm'
 
-python3 ./for_supervisor.py
+python3 ./for_supervisor.py > "$LOG_FILE" 2>&1
